ddc00e628a14226e6b8cbf0eeedcc2b159057ada
manishrawat0212 <manishrawat0212@gmail.com>
2017-06-02 18:02:41 +0530

    config details added in deploy.rb and production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,12 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+set :rails_env, fetch(:stage)
+set :deploy_to, "/var/www/spree_depot"
+set :branch, ENV.fetch('branch', 'master')
+
+server "34.211.92.176", user: "deploy"
+
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
