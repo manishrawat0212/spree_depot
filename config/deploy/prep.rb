da8d258f71079da1b53ba4a9051075c683bdc461
manishrawat0212 <manishrawat0212@gmail.com>
2017-06-09 13:05:20 +0530

    roles specified in prep.rb

--- config/deploy/prep.rb
+++ config/deploy/prep.rb
@@ -2,4 +2,4 @@ set :rails_env, fetch(:stage)
 set :deploy_to, "/var/www/spree_depot"
 set :branch, ENV.fetch('branch', 'master')
 
-server "34.211.92.176", user: "deploy"
+server "34.211.92.176", roles: %w{app db delayed_job}, user: "deploy"
